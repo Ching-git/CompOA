21f6228a6ccd30d238b4bb765c53c08fa79fea31
Ching <gaorong_q@163.com>
2015-07-31 18:29:00 +0800

    03 day finish

--- src/cn/edu/csu/oa/serviceImpl/UserServiceImpl.java
+++ src/cn/edu/csu/oa/serviceImpl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package cn.edu.csu.oa.serviceImpl;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,5 +11,15 @@ import cn.edu.csu.oa.service.UserService;
 @Service
 @Transactional
 public class UserServiceImpl extends DaoSupportImpl<User> implements UserService {
+
+	@Override
+	public void save(User user) {
+		//默认密码是1234
+		String md5 = DigestUtils.md5Hex("1234");
+		user.setPassword(md5);
+		
+		//保存到数据库
+		getSession().save(user);
+	}
  
 }
--- src/cn/edu/csu/oa/view/UserAction.java
+++ src/cn/edu/csu/oa/view/UserAction.java
@@ -3,6 +3,7 @@ package cn.edu.csu.oa.view;
 import java.util.HashSet;
 import java.util.List;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
@@ -17,7 +18,7 @@ import com.opensymphony.xwork2.ActionContext;
 @SuppressWarnings("serial")
 @Controller
 @Scope("prototype")
-public class UserAction extends BaseAction<User>{
+public class UserAction extends BaseAction<User> {
 
 	private Long departmentId;
 	private Long[] roleIds;
@@ -39,7 +40,8 @@ public class UserAction extends BaseAction<User>{
 	public String addUI() throws Exception {
 		// 准备数据：departmentList
 		List<Department> topList = departmentService.findTopList();
-		List<Department> departmentList = DepartmentUtils.getAllDepartmentList(topList, null);
+		List<Department> departmentList = DepartmentUtils.getAllDepartmentList(
+				topList, null);
 		ActionContext.getContext().put("departmentList", departmentList);
 
 		// 准备数据：roleList
@@ -66,68 +68,68 @@ public class UserAction extends BaseAction<User>{
 
 	/** 修改页面 */
 	public String editUI() throws Exception {
-		// // 准备回显的数据
-		// User user = userService.getById(model.getId());
-		// ActionContext.getContext().getValueStack().push(user);
-		// // >> 处理部门
-		// if (user.getDepartment() != null) {
-		// departmentId = user.getDepartment().getId();
-		// }
-		// // >> 处理岗位
-		// roleIds = new Long[user.getRoles().size()];
-		// int index = 0;
-		// for (Role role : user.getRoles()) {
-		// roleIds[index++] = role.getId();
-		// }
-		//
-		// // 准备数据：departmentList
-		// List<Department> topList = departmentService.findTopList();
-		// List<Department> departmentList =
-		// DepartmentUtils.getAllDepartmentList(topList, null);
-		// ActionContext.getContext().put("departmentList", departmentList);
-		//
-		// // 准备数据：roleList
-		// List<Role> roleList = roleService.findAll();
-		// ActionContext.getContext().put("roleList", roleList);
+		// 准备回显的数据
+		User user = userService.getById(model.getId());
+		ActionContext.getContext().getValueStack().push(user);
+		// >> 处理部门
+		if (user.getDepartment() != null) {
+			departmentId = user.getDepartment().getId();
+		}
+		// >> 处理岗位
+		roleIds = new Long[user.getRoles().size()];
+		int index = 0;
+		for (Role role : user.getRoles()) {
+			roleIds[index++] = role.getId();
+		}
+
+		// 准备数据：departmentList
+		List<Department> topList = departmentService.findTopList();
+		List<Department> departmentList = DepartmentUtils.getAllDepartmentList(
+				topList, null);
+		ActionContext.getContext().put("departmentList", departmentList);
+
+		// 准备数据：roleList
+		List<Role> roleList = roleService.findAll();
+		ActionContext.getContext().put("roleList", roleList);
 
 		return "saveUI";
 	}
 
 	/** 修改 */
 	public String edit() throws Exception {
-		// // 1，从数据库中取出原对象
-		// User user = userService.getById(model.getId());
-		//
-		// // 2，设置要修改的属性
-		// user.setLoginName(model.getLoginName());
-		// user.setName(model.getName());
-		// user.setGender(model.getGender());
-		// user.setPhoneNumber(model.getPhoneNumber());
-		// user.setEmail(model.getEmail());
-		// user.setDescription(model.getDescription());
-		// // >> 处理关联的一个部门
-		// user.setDepartment(departmentService.getById(departmentId));
-		// // >> 处理关联的多个岗位
-		// List<Role> roleList = roleService.getByIds(roleIds);
-		// user.setRoles(new HashSet<Role>(roleList));
-		//
-		// // 3，更新到数据库
-		// userService.update(user);
+		// 1，从数据库中取出原对象
+		User user = userService.getById(model.getId());
+
+		// 2，设置要修改的属性
+		user.setLoginName(model.getLoginName());
+		user.setName(model.getName());
+		user.setGender(model.getGender());
+		user.setPhoneNumber(model.getPhoneNumber());
+		user.setEmail(model.getEmail());
+		user.setDescription(model.getDescription());
+		// >> 处理关联的一个部门
+		user.setDepartment(departmentService.getById(departmentId));
+		// >> 处理关联的多个岗位
+		List<Role> roleList = roleService.getByIds(roleIds);
+		user.setRoles(new HashSet<Role>(roleList));
+
+		// 3，更新到数据库
+		userService.update(user);
 
 		return "toList";
 	}
 
 	/** 初始化密码为1234 */
 	public String initPassword() throws Exception {
-		// // 1，从数据库中取出原对象
-		// User user = userService.getById(model.getId());
-		//
-		// // 2，设置要修改的属性
-		// String md5 = DigestUtils.md5Hex("1234"); // 密码要使用MD5摘要
-		// user.setPassword(md5);
-		//
-		// // 3，更新到数据库
-		// userService.update(user);
+		 // 1，从数据库中取出原对象
+		 User user = userService.getById(model.getId());
+		
+		 // 2，设置要修改的属性
+		 String md5 = DigestUtils.md5Hex("1234"); // 密码要使用MD5摘要
+		 user.setPassword(md5);
+		
+		 // 3，更新到数据库
+		 userService.update(user);
 
 		return "toList";
 	}
